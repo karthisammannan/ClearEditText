0bb713f3dd4b9f1323bf5c6a88fbfd557a1875d1
ClearEditText <yourskarthi2@gmail.com>
2018-03-05 17:48:15 +0530

    Clear edittext first commit

---
+++ app/src/main/java/com/example/scheduler/library/ClearEditText.java
@@ -0,0 +1,106 @@
+package com.example.scheduler.library;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.example.scheduler.R;
+
+/**
+ * Created by Karthi on 5/3/2018.
+ */
+
+public class ClearEditText extends AppCompatEditText implements View.OnTouchListener, View.OnFocusChangeListener, TextWatcher {
+    private Drawable mClearTextIcon;
+    private OnFocusChangeListener mOnFocusChangeListener;
+    private OnTouchListener mOnTouchListener;
+    public ClearEditText(Context context) {
+        super(context);
+        init(context);
+    }
+    public ClearEditText(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public ClearEditText(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+    private void init(final Context context) {
+        final Drawable drawable = ContextCompat.getDrawable(context, R.drawable.ic_close);
+        final Drawable wrappedDrawable = DrawableCompat.wrap(drawable); //Wrap the drawable so that it can be tinted pre Lollipop
+        DrawableCompat.setTint(wrappedDrawable, getCurrentHintTextColor());
+        mClearTextIcon = wrappedDrawable;
+        mClearTextIcon.setBounds(0, 0, mClearTextIcon.getIntrinsicHeight(), mClearTextIcon.getIntrinsicHeight());
+        setClearIconVisible(false);
+        super.setOnTouchListener(this);
+        super.setOnFocusChangeListener(this);
+        addTextChangedListener(this);
+    }
+    private void setClearIconVisible(final boolean visible) {
+        mClearTextIcon.setVisible(visible, false);
+        final Drawable[] compoundDrawables = getCompoundDrawables();
+        setCompoundDrawables(
+                compoundDrawables[0],
+                compoundDrawables[1],
+                visible ? mClearTextIcon : null,
+                compoundDrawables[3]);
+    }
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        if (isFocused()) {
+            setClearIconVisible(charSequence.length() > 0);
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+
+    @Override
+    public void onFocusChange(View view, boolean hasFocus) {
+        if (hasFocus) {
+            setClearIconVisible(getText().length() > 0);
+        } else {
+            setClearIconVisible(false);
+        }
+        if (mOnFocusChangeListener != null) {
+            mOnFocusChangeListener.onFocusChange(view, hasFocus);
+        }
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        final int x = (int) motionEvent.getX();
+        if (mClearTextIcon.isVisible() && x > getWidth() - getPaddingRight() - mClearTextIcon.getIntrinsicWidth()) {
+            if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                setText("");
+            }
+            return true;
+        }
+        return mOnTouchListener != null && mOnTouchListener.onTouch(view, motionEvent);
+    }
+    @Override
+    public void setOnFocusChangeListener(final OnFocusChangeListener onFocusChangeListener) {
+        mOnFocusChangeListener = onFocusChangeListener;
+    }
+
+    @Override
+    public void setOnTouchListener(final OnTouchListener onTouchListener) {
+        mOnTouchListener = onTouchListener;
+    }
+}
